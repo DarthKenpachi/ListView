6b0ff56cdaa589a25a949081a2632e2826d6e626
DarthKenpachi <darth9r0t3u5@outlook.com>
2015-11-24 00:35:12 -0500

    Initial commit
    Ver 1.0
    11/24/2015

---
+++ app/src/main/java/com/m5b10/listview/MainActivity.java
@@ -0,0 +1,77 @@
+package com.m5b10.listview;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] favoriteTVShows = {"Star Trek","Star Trek TNG","Star Trek DSN","Star Trek Voyager",
+                "Andromeda","Babylon 5","Simpsons","Afro Samurai","Ninja Scroll","Assassin"};   //data to put in ListView
+        //create list adapter to adapt array & generate rows of ListView
+        ListAdapter theAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,favoriteTVShows);//this, exampleLayout,array obj
+
+        ListView mainListView = (ListView) findViewById(R.id.mainTextView);    //cast ListView
+
+        mainListView.setAdapter(theAdapter); //pass data (theAdapter containing the adapted String[]) to listView obj
+
+        mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                String tvShowPicked = "You selected " + String.valueOf(adapterView.getItemAtPosition(position));
+
+                Toast.makeText(MainActivity.this, tvShowPicked, Toast.LENGTH_SHORT).show(); //toaster pop of mainListView item sel
+
+            }
+        });
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Kandy makes me Dandy.", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
